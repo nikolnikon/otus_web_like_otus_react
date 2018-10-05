35c618494949bbdf82fe34ad2dc8792049ae088e
nikolnikon <nikolnikon@gmail.com>
2018-10-06 01:39:02 +0300

    Добавил scripts под контроль версий, сдлал все файлы scss, прописал для них loaders

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -166,7 +166,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.scss$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -205,6 +205,7 @@ module.exports = {
                         ],
                       },
                     },
+                    require.resolve('sass-loader'),
                   ],
                 },
                 extractTextPluginOptions
@@ -212,14 +213,7 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
-            {
-              test: /\.scss$/,
-              loaders: [
-                  require.resolve('style-loader'),
-                  require.resolve('css-loader'),
-                  require.resolve('sass-loader'),
-              ]
-            },
+
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
--- src/user/RegistrationForm.js
+++ src/user/RegistrationForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './RegistrationForm.css'
+import './RegistrationForm.scss'
 import Form from '../common/Form'
 
 class RegistrationForm extends Component {
